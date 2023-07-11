91e6e2d4428f96d28a1f1587e3ab812213a3629e
Kelbaz <81097895+kelbazz@users.noreply.github.com>
2023-07-11 23:48:56 +0200

    Update module.js

--- module.js
+++ module.js
@@ -1,4 +1,13 @@
-export default async function fetchTest() {
-    console.log("Hello");
+export async function fetchTest() {
     return (await fetch("kelbazz.github.io").then(r => r.text()));
+}
+export async function logTest() {
+     console.log("Hello");
+     console.info("Hello");
+     console.error("Hello");
+     console.warn("Hello");
+     console.table([
+        [1, 2],
+        [3, 4],
+    ]);
 }
